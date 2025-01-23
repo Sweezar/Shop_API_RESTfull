277d4bdc9d3b63b0725e67b25288864e722e1f79
Sweezar <royal13flush@gmail.com>
2025-01-23 11:59:38 +0300

    add swagger anotations in client controller

--- src/REST-API/src/main/java/org/berneick/controller/ClientController.java
+++ src/REST-API/src/main/java/org/berneick/controller/ClientController.java
@@ -1,5 +1,10 @@
 package org.berneick.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.berneick.exception.ClientNotFoundException;
 import org.berneick.model.AddressDTO;
@@ -15,19 +20,26 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/clients")
+@Tag(name = "Client API", description = "API для управления клиентами")
 public class ClientController {
     @Autowired
     private ClientService clientService;
 
     @PostMapping
+    @Operation(summary = "Создание нового клиента", description = "Создает нового пользователя с предоставленными данными.")
+    @ApiResponse(responseCode = "201", description = "Клиент успешно создан")
     public ResponseEntity<ClientDTO> createClient(@Valid @RequestBody ClientDTO clientDTO) {
         ClientDTO createdClient = clientService.createClient(clientDTO);
         return new ResponseEntity<>(createdClient, HttpStatus.CREATED);
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Получение клиента по имени и фамилии", description = "Возвращает информацию о клиенте по его имени и фамилии.")
+    @ApiResponse(responseCode = "200", description = "Клиенты найдены")
     public ResponseEntity<List<ClientDTO>> getClientsByNameAndSurname(
+            @Parameter(description = "Имя пользователя", example = "Иван")
             @RequestParam String name,
+            @Parameter(description = "Фамилия пользователя", example = "Иванов")
             @RequestParam String surname
     ) {
         List<ClientDTO> clients = clientService.findByClientNameAndClientSurname(name, surname);
@@ -36,8 +48,12 @@ public class ClientController {
     }
 
     @GetMapping
+    @Operation(summary = "Получение всех клиентов", description = "Возвращает информацию о клиентах.")
+    @ApiResponse(responseCode = "200", description = "Клиенты найдены")
     public ResponseEntity<List<ClientDTO>> getAllClients(
+            @Parameter(description = "Лимит возвращаемых значений", example = "10")
             @RequestParam(defaultValue = "10") int limit,
+            @Parameter(description = "Количество значений, которое стоит пропустить", example = "0")
             @RequestParam(defaultValue = "0") int offset
     ) {
         List<ClientDTO> clients = clientService.findAll(limit, offset);
@@ -46,8 +62,15 @@ public class ClientController {
     }
 
     @PatchMapping("/{clientId}")
+    @Operation(summary = "Изменение адреса клиента", description = "Изменяет адрес клиента по его id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Изменение адреса успешно"),
+            @ApiResponse(responseCode = "404", description = "Клиент с таким id не найден")
+    })
     public ResponseEntity<?> updateAddress(
+            @Parameter(description = "Id клиента", example = "99b01137-6380-4684-a3f9-1ebaebc33060")
             @PathVariable UUID clientId,
+            @Parameter(description = "Новый адрес")
             @Valid @RequestBody AddressDTO addressDTO
     ) {
             try {
@@ -59,7 +82,15 @@ public class ClientController {
     }
 
     @DeleteMapping("/{clientId}")
-    public ResponseEntity<Void> deleteClient(@PathVariable UUID clientId) {
+    @Operation(summary = "Удаление клиента", description = "Удаляет клиента по его id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Клиент удален"),
+            @ApiResponse(responseCode = "404", description = "Клиент с таким id не найден")
+    })
+    public ResponseEntity<Void> deleteClient(
+            @Parameter(description = "Id клиента", example = "99b01137-6380-4684-a3f9-1ebaebc33060")
+            @PathVariable UUID clientId
+    ) {
         try {
             clientService.delete(clientId);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/REST-API/src/main/java/org/berneick/model/ClientDTO.java
+++ src/REST-API/src/main/java/org/berneick/model/ClientDTO.java
@@ -1,5 +1,6 @@
 package org.berneick.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -13,22 +14,31 @@ import java.util.UUID;
 @Getter
 @Setter
 @Builder
+@Schema(description = "Модель клиента")
 public class ClientDTO {
+    @Schema(description = "Уникальный идентификатор клиента", example = "99b01137-6380-4684-a3f9-1ebaebc33060")
     private UUID clientId;
 
     @NotBlank
+    @Schema(description = "Имя клиента", example = "Иван")
     private String clientName;
 
     @NotBlank
+    @Schema(description = "Фамилия клиента", example = "Иванов")
     private String clientSurname;
 
     @NotNull
+    @Schema(description = "Дата рождения клиента", example = "2025-01-23")
     private LocalDate birthday;
 
     @NotBlank
+    @Schema(description = "Пол клиента", example = "male")
     private String gender;
 
     @NotNull
+    @Schema(description = "Дата регистрации клиента", example = "2025-01-23T08:49:30")
     private LocalDateTime registrationDate;
+
+    @Schema(description = "Адрес клиента", example = "99b01137-6380-4684-a3f9-1ebaebc33061")
     private UUID addressId;
 }
